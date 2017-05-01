a01002847359026d1ebf05299127438d8eab8503
Jakub Elżbieciak <jelz@post.pl>
2017-05-01 10:18:41 +0200

    Use CMA to get list of CTs

--- test/client.test.js
+++ test/client.test.js
@@ -4,7 +4,7 @@ const test = require('tape');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru();
 
-const httpStub = {get: sinon.stub()};
+const httpStub = {get: sinon.stub().resolves({items: []})};
 const createHttpClientStub = sinon.stub().returns(httpStub);
 const createEntryLoaderStub = sinon.stub();
 
@@ -13,26 +13,63 @@ const createClient = proxyquire('../src/client.js', {
   './entry-loader.js': createEntryLoaderStub
 });
 
-const client = createClient({spaceId: 'SPID', cdaToken: 'token'});
-
-const expectedHttpClientConfig = {
-  base: 'https://cdn.contentful.com/spaces/SPID',
-  headers: {Authorization: 'Bearer token'}
+const config = {
+  spaceId: 'SPID',
+  cdaToken: 'CDA-TOKEN',
+  cmaToken: 'CMA-TOKEN'
 };
 
-test('client: getting content types', function (t) {
+const client = createClient(config);
+
+test('client: config options', function (t) {
   t.plan(4);
+
+  const p1 = client.getContentTypes();
+  client.createEntryLoader();
+
+  const alt = {secure: false, domain: 'altdomain.com'};
+  const c2 = createClient(Object.assign(alt, config));
+  const p2 = c2.getContentTypes();
+  c2.createEntryLoader();
+
+  Promise.all([p1, p2]).then(() => {
+    t.deepEqual(createHttpClientStub.firstCall.args, [{
+      base: 'https://api.contentful.com/spaces/SPID',
+      headers: {Authorization: 'Bearer CMA-TOKEN'}
+    }]);
+
+    t.deepEqual(createHttpClientStub.secondCall.args, [{
+      base: 'https://cdn.contentful.com/spaces/SPID',
+      headers: {Authorization: 'Bearer CDA-TOKEN'}
+    }]);
+
+    t.deepEqual(createHttpClientStub.thirdCall.args, [{
+      base: 'http://api.altdomain.com/spaces/SPID',
+      headers: {Authorization: 'Bearer CMA-TOKEN'}
+    }]);
+
+    t.deepEqual(createHttpClientStub.lastCall.args, [{
+      base: 'http://cdn.altdomain.com/spaces/SPID',
+      headers: {Authorization: 'Bearer CDA-TOKEN'}
+    }]);
+  });
+});
+
+test('client: getting content types', function (t) {
+  t.plan(3);
   httpStub.get.resolves({items: [1, {}, 3]});
 
-  const p = client.getContentTypes()
+  client.getContentTypes()
   .then(cts => {
-    t.deepEqual(createHttpClientStub.firstCall.args, [expectedHttpClientConfig]);
     t.deepEqual(httpStub.get.firstCall.args, ['/content_types', {limit: 1000}]);
     t.deepEqual(cts, [1, {}, 3]);
-  });
 
-  Promise.all([p, client.getContentTypes()])
-  .then(() => t.equal(createHttpClientStub.callCount, 2));
+    return Promise.all([
+      Promise.resolve(createHttpClientStub.callCount),
+      client.getContentTypes()
+    ]);
+  })
+  .then(([count]) => t.equal(createHttpClientStub.callCount, count+1));
 });
 
 test('client: entry loader creation', function (t) {
@@ -40,7 +77,6 @@ test('client: entry loader creation', function (t) {
   createEntryLoaderStub.returns(entryLoader);
 
   t.equal(client.createEntryLoader(), entryLoader);
-  t.deepEqual(createHttpClientStub.lastCall.args, [expectedHttpClientConfig]);
   t.deepEqual(createEntryLoaderStub.firstCall.args, [httpStub]);
 
   t.end();
